e5c579967710d7257c2422b119b1edea02a9eed6
RBaird2001 <rbaird2001@yahoo.com>
2019-08-04 18:43:01 -0600

    Get Draggable to return to originalposition

---
+++ return draggable to original position.js
@@ -0,0 +1,13 @@
+Draggable items don't keep track of their original position that I know of; only during drag and to be snapped back. You can just do this on your own, though:
+
+$("#draggable").data({
+    'originalLeft': $("#draggable").css('left'),
+    'origionalTop': $("#draggable").css('top')
+});
+
+$(".reset").click(function() {
+    $("#draggable").css({
+        'left': $("#draggable").data('originalLeft'),
+        'top': $("#draggable").data('origionalTop')
+    });
+});
